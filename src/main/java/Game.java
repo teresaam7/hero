426cfabc15a550f37824b3dce25410bd2754e098
teresaam7 <teresamas2010@gmail.com>
2023-09-26 12:33:56 +0100

    Commit for the Reading KeyStrokes phase

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,5 @@
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -7,6 +9,8 @@ import com.googlecode.lanterna.TextCharacter;
 import java.io.IOException;
 public class Game {
     private static Screen screen;
+    private int x = 10;
+    private int y = 10;
     // Default constructor
     public Game(){
         // This code initializes a Lanterna Terminal and a Screen
@@ -22,10 +26,36 @@ public class Game {
     }
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
     public void run() throws IOException{
-        draw();
+        while (true){
+            draw();
+            // Method waits for a key stroke
+            KeyStroke key = screen.readInput();
+            processKey(key);
+            if (key.getKeyType() == KeyType.EOF || (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q')){
+                break;
+            }
+        }
+        screen.close();
+
+    }
+    private void processKey(KeyStroke key) {
+        switch (key.getKeyType()){
+            case ArrowRight:
+                x++;
+                break;
+            case ArrowLeft:
+                x--;
+                break;
+            case ArrowDown:
+                y++;
+                break;
+            case ArrowUp:
+                y--;
+                break;
+        }
     }
 }
