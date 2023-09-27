874705a14df8f21102e5a46bbbd16a9c7f5fc88a
teresaam7 <teresamas2010@gmail.com>
2023-09-27 22:19:37 +0100

    Correction of the Collision Detector method

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -20,4 +20,12 @@ public class Position {
     public void setY(int y) {
         this.y = y;
     }
+    // Method declared by the Object class
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (getClass() != o.getClass()) return false;
+        Position p = (Position) o;
+        return x == p.getX() && y == p.getY();
+    }
 }
