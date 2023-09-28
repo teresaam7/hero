8e1d9140ced0b8d01900b5c4259100935e698d56
teresaam7 <teresamas2010@gmail.com>
2023-09-28 06:56:31 +0100

    Submission of the implementation of the Coins step

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -6,20 +6,21 @@ import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Arena {
     private int width;
     private int height;
     private Hero hero;
     private List<Wall> walls;
+    private List<Coin> coins;
     public Arena(int width, int height){
         this.width = width;
         this.height = height;
         // Starting position in the middle
         this.hero = new Hero(width / 2, height / 2);
         this.walls = createWalls();
+        this.coins = createCoins();
     }
     public boolean canHeroMove(Position position){
         int x = position.getX();
@@ -36,8 +37,10 @@ public class Arena {
         return true;
     }
     public void moveHero(Position position) {
-        if (canHeroMove(position))
+        if (canHeroMove(position)){
             hero.setPosition(position);
+            retrieveCoins();
+        }
     }
     public void processKey(KeyStroke key) {
         switch (key.getKeyType()){
@@ -67,13 +70,60 @@ public class Arena {
         }
         return walls;
     }
+    private List<Coin> createCoins() {
+        Random random = new Random();
+        ArrayList<Coin> coins = new ArrayList<>();
+        // Creation of a list to track existing positions
+        List<Position> candidatePositions = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            // Generation of a random position for the coin
+            Position coinPosition = getRandomPosition(random);
+            // Verify if the coin is colliding with the hero and with other coins
+            while (candidatePositions.contains(coinPosition) || coinPosition.equals(hero.getPosition())) {
+                coinPosition = getRandomPosition(random);
+            }
+            coins.add(new Coin(coinPosition.getX(), coinPosition.getY()));
+            candidatePositions.add(coinPosition);
+        }
+        return coins;
+    }
+    private Position getRandomPosition(Random random) {
+        int cX = random.nextInt(width - 2) + 1;
+        int cY = random.nextInt(height - 2) + 1;
+        return new Position(cX, cY);
+    }
+    private boolean collisionCoins(int x, int y){
+        for (Coin coin: coins){
+            if (coin.getPosition().getX() == x && coin.getPosition().getY() == y){
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean collisionHero(int x, int y){
+        return (hero.getPosition().getX() == x && hero.getPosition().getY() == y);
+    }
+    public void retrieveCoins(){
+        Iterator<Coin> iterator = coins.iterator();
+        while (iterator.hasNext()) {
+            Coin coin = iterator.next();
+            if (coin.getPosition().equals(hero.getPosition())) {
+                iterator.remove();
+                break;
+            }
+        }
+    }
     public void draw(TextGraphics graphics) throws IOException {
         // Drawing the arena
-        graphics.setBackgroundColor(TextColor.Factory.fromString("#57cef5"));
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#87ceeb"));
         graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
         // Drawing the walls
         for (Wall wall : walls) {
             wall.draw(graphics);
+        }
+        // Drawing the coins
+        for (Coin coin : coins){
+            coin.draw(graphics);
         }
         // Drawing the hero
         hero.draw(graphics);
---
+++ src/main/java/Coin.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public class Coin extends Element{
+    public Coin(int x, int y){
+        super(x, y);
+    }
+    public void draw(TextGraphics graphics){
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FFD700"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(getPosition().getX(), getPosition().getY()), "$");
+    }
+}
